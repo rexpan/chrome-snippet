9162143fb1d3b0908d293a1daeee45ddd535b37b
Rex Pan <napxer@gmail.com>
2020-02-26 08:52:23 +0700

    www.udemy.com.user.js

--- www.udemy.com.user.js
+++ www.udemy.com.user.js
@@ -2,10 +2,10 @@
 // @id              udemy
 // @name            udemy auto add free
 // @namespace       http://tampermonkey.net/
-// @version         2019.12.10
+// @version         2020.02.26
 // @description     auto open Udemy link
 // @author          Rex Pan <napxer@gmail.com>
-// @match           https://www.udemy.com/*
+// @match           https://www.udemy.com/course/*
 // @grant           window.close
 // @updateURL       https://github.com/rexpan/chrome-snippet/raw/master/www.udemy.com.user.js
 // @downloadURL     https://github.com/rexpan/chrome-snippet/raw/master/www.udemy.com.user.js
@@ -16,28 +16,59 @@
 // @license         MIT
 // ==/UserScript==
 
-;(async () => { "use strict";
+;(() => { "use strict";
 
 console.clear();
 
-const buyBtnSelector = `button[data-purpose="buy-this-course-button"]`;
-const a2cSelector = `button[data-purpose="add-to-cart"]`;
-const oldPriceSelector = `.buy-box [data-purpose="course-old-price-text"]`;
-
-const buyBtn = await querySelectorAsync(buyBtnSelector);
-const text = buyBtn.innerText.trim().toLowerCase();
-if (text == "go to course") {
-    window.close();
-    return;
-}
-
-if (document.querySelector(oldPriceSelector) == null) {
+if (!location.search.includes("couponCode=")) {
+    debugger;
     window.close();
 }
 
-const a2cBtn = document.querySelector(a2cSelector);
-if (a2cBtn.innerText.trim().toLowerCase() == "add to cart") {
-    a2cBtn.click();
+(async () => {
+    const buyBtn = await querySelectorAsync(`button[data-purpose="buy-this-course-button"]`);
+    if (isPurchased(buyBtn)) {
+        debugger;
+        window.close();
+    }
+    if (document.querySelector(`.buy-box [data-purpose="course-old-price-text"]`) == null) {
+        window.close();
+    }
+
+    const addToCartBtn = document.querySelector(`.buy-box__element.buy-box__element--add-to-cart-button`);
+    if (addToCartBtn.innerText.trim().toLowerCase() == "add to cart") {
+        addToCartBtn.click();
+    }
+})();
+
+(async () => {
+    const buyBtn = await querySelectorAsync(`[data-purpose="purchase-section"] .buy-box__element--buy-button`);
+    if (isPurchased(buyBtn)) {
+        debugger;
+        window.close();
+    }
+
+    const priceTag = await querySelectorAsync(`[data-purpose="purchase-section"] [data-purpose="course-price-text"]`);
+    removeScreenReader();
+    const price = priceTag.innerText.trim().toLowerCase();
+    if (price != "free") {
+        if (price.includes("$")) window.close();
+        else { debugger; return; }
+    }
+
+    const addToCartBtn = document.querySelector(`[data-purpose="purchase-section"] .buy-box__element--add-to-cart-button button`);
+    if (addToCartBtn == null) debugger;
+    else addToCartBtn.click();
+})();
+
+function isPurchased(buyBtn) {
+    const text = buyBtn.innerText.trim().toLowerCase();
+    return (text == "go to course");
+}
+
+function removeScreenReader() {
+    const xs = document.querySelectorAll(`.udlite-sr-only`);
+    for (const x of xs) x.remove();
 }
 
 async function querySelectorAsync(selector) {
